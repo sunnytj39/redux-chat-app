db6a9e4fc1fb0d905c4b32c5318da0fc4d0001d3
Kenta Kato <sunnytj39@gmail.com>
2018-03-29 17:23:48 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from 'components/App';
